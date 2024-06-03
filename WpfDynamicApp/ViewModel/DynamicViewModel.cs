1b6ff5d2b759ddd3c11e3d7674cd0fca1425c7b4
Gilbert6137 <gilbert@mblock.com.tw>
2024-06-03 10:29:49 +0800

    加入專案檔案。

---
+++ WpfDynamicApp/ViewModel/DynamicViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+
+namespace WpfDynamicApp.ViewModel
+{
+    public class DynamicViewModel : DynamicObject, INotifyPropertyChanged
+    {
+        private readonly Dictionary<string, object> _dynamicProperties = new Dictionary<string, object>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            _dynamicProperties[binder.Name] = value;
+            OnPropertyChanged(binder.Name);
+            return true;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return _dynamicProperties.TryGetValue(binder.Name, out result);
+        }
+
+        public void SetDynamicProperty<T>(string propertyName, T value)
+        {
+            if (!_dynamicProperties.ContainsKey(propertyName))
+            {
+                _dynamicProperties[propertyName] = value;
+                OnPropertyChanged(propertyName);
+            }
+            else
+            {
+                _dynamicProperties[propertyName] = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+
+        public T GetDynamicProperty<T>(string propertyName)
+        {
+            if (_dynamicProperties.TryGetValue(propertyName, out var value))
+            {
+                return (T)value;
+            }
+            return default(T);
+        }
+    }
+}
+
+
+
+
---
+++ WpfDynamicApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfDynamicApp.ViewModel;
+
+namespace WpfDynamicApp.ViewModel
+{
+    public delegate void LoadItemsDelegate();
+    public class MainViewModel : DynamicViewModel
+    {
+        public ICommand LoadItemsCommand { get; }
+
+        public MainViewModel()
+        {
+            LoadItemsCommand = new RelayCommand(_ => LoadItems());
+        }
+
+        private void LoadItems()
+        {
+            var items = new ObservableCollection<string> { "DynamicItem1", "DynamicItem2", "DynamicItem3" };
+            SetDynamicProperty("ItemsSource", items);
+
+            // Assume selecting the first item as default
+            SetDynamicProperty("SelectedItem", items.FirstOrDefault());
+        }
+
+    }
+
+}
+
+
+
+
+
